ff22a19b5ae166c0f4f119942e58338b8a86ed1e
quil <quil735@gmail.com>
2022-12-11 13:17:11 -0700

    take two on the player bounds

---
+++ Assets/Scripts/PlayerBounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerBounds : MonoBehaviour
+{
+    public TextMeshProUGUI turnAround;
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("terrain"))
+        {
+            turnAround.text = "TURN BACK";
+        }
+
+        transform.Rotate(0, 180, 0);
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5f);
+        turnAround.text = "";
+    }
+}
